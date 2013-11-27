9e7175ae41c83661f1ebf45619e4d10c9833163d
saidnet <saidnet212@gmail.com>
2013-11-27 12:15:53 +0000

    version 1.0.0.3

--- Projet/controleurs/connexion.php
+++ Projet/controleurs/connexion.php
@@ -1,7 +1,11 @@
 <?php
 	if(isset($_POST['connecter'])){
-		$_SESSION['login']='irisi2';
-		header("Location: index.php");
+		if($_POST['username']=='irisi2' AND $_POST['password']=='wideteam' ){
+			$_SESSION['login']='irisi2';
+			header("Location: index.php");
+		}
+		else
+			$tpl->display('connexion.htm');
 	}
 	else
 		$tpl->display('connexion.htm');
