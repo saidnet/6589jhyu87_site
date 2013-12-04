0692fa5b9879b416258d107dc7c7897b9d3a3fed
saidnet <saidnet212@gmail.com>
2013-12-04 15:03:07 +0000

    version 1.0.1.7

--- Projet/controleurs/commissariat.php
+++ Projet/controleurs/commissariat.php
@@ -1,2 +1,9 @@
 <?php
-$tpl->display('commissariat.htm');
+if($_GET['action']=='update'){
+
+}
+else{
+	$r=$bdd_manager->getList('commissariat' );
+	$tpl->assign('list',$r);
+	$tpl->display('commissariat.htm');
+}
